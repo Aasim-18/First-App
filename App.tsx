d5f546be5c6812d9b577035e290a9ae8a7b2c1c1
Syed Aasim <aasimsyed398@gmail.com>
2025-09-30 01:07:57 +0530

    Password generator Setup

--- App.tsx
+++ App.tsx
@@ -1,17 +1,33 @@
-
-
-import { NewAppScreen } from '@react-native/new-app-screen';
+import React, {useState} from 'react';
 import { StatusBar, StyleSheet, useColorScheme, View, Text } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+import * as yup from "yup";
+
+const PasswordSchema = yup.object().shape({
+  PasswordLength: yup.number()
+  .min(4, "Password length should be at least 4 characters")
+  .max(16, "Password length should not be greater than 16 characters")
+  .required("Password length is required"),
+})
+
+
 
 function App() {
  return <View>
-  <Text>Chal Ja Bhai</Text>
+  <Text style={styles.HeadingText}>Password Generator</Text>
   </View>
 }
+
+const styles = StyleSheet.create({
+  HeadingText: {
+    fontWeight: 700,
+    color: "#fff",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    fontSize: 24,
+    
+  }
+})
 
 
 
